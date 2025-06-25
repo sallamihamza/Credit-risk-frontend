0026912f431b198493aea4a2ae636aab4da1dabe
sallamihamza <88661606+sallamihamza@users.noreply.github.com>
2025-06-25 23:12:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Data-Science-Projects/',
+  base: '/Credit-risk-frontend/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
